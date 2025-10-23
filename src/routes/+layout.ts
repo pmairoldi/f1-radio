d2b596460465e3dff1fcb50cff84c26047560f55
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2025-10-22 21:52:05 -0400

    Add analytics (#23)
    * add posthog
    
    * update deps
    
    * update packagemanager
    
    * update package manager
    
    * update pnpm
    
    * remove pnpm lock
    
    * update lock file
    
    * update packages
    
    * update example .env

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,3 @@
-import { dev } from '$app/environment';
-import { inject } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+import { initPosthog } from '../lib/posthog/client';
 
-inject({ mode: dev ? 'development' : 'production' });
-injectSpeedInsights();
+initPosthog();
